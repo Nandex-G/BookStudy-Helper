56b97972fa406f81a557093ed7238f445cb0ffac
Nandex <hosseinspaceh13781389@gmail.com>
2025-02-14 22:39:05 +0330

    Continue rewriting, add pages, create a new Book Selection bar, some changes in buttons

---
+++ pages/js/book.js
@@ -0,0 +1,43 @@
+// Book Pages =======================================================
+
+let bookslist = document.querySelectorAll('.bookSection_body span');
+let activeBook = null;
+
+bookslist.forEach(book => {
+    book.addEventListener('click', (event) => {
+        event.stopPropagation();
+        let buttons = book.querySelectorAll('button');
+        let paragraph = book.querySelector('p');
+
+        if (activeBook && activeBook !== book) {
+            resetBook(activeBook);
+        }
+
+        let isActive = book.classList.contains('bookActiveWidth');
+
+        if (!isActive) {
+            book.classList.add('bookActiveWidth');
+            buttons.forEach(btn => btn.classList.add('bookActiveButtons'));
+            paragraph.classList.add('bookActiveP');
+            activeBook = book;
+        } else {
+            resetBook(book);
+        }
+    });
+});
+
+document.addEventListener('click', () => {
+    if (activeBook) {
+        resetBook(activeBook);
+        activeBook = null;
+    }
+});
+
+function resetBook(book) {
+    book.classList.remove('bookActiveWidth');
+    let buttons = book.querySelectorAll('button');
+    let paragraph = book.querySelector('p');
+
+    buttons.forEach(btn => btn.classList.remove('bookActiveButtons'));
+    paragraph.classList.remove('bookActiveP');
+}
--- script.js
+++ script.js
@@ -4,43 +4,89 @@ let isSectionOpen = false
 let navSelectionTitle_element = document.querySelector('.navSelection-title')
 let navSelection_element = document.querySelector('.navSelection')
 let navSelection_body_element = document.querySelector('.navSelection-body')
+let navSelection_backBTN_element = document.querySelector('.navSelection-backArrow')
 let navSelection_BTN_element = document.querySelector('.navSelection-BTN')
 
+let navSelection_body_element__SAVE
+let navSelectionTitle_element__SAVE
+let isSubBook_condition
 
 let books = {
-    'cambridge' : ['Cambridge_10' , 'Cambridge_11' , 'Cambridge_12' , 'Cambridge_13' , 'Cambridge_14' , 'Cambridge_15' , 'Cambridge_16' , 'Cambridge_17' , 'Cambridge_18' , 'Cambridge_19' , 'Cambridge_20'],
-    'collins' : ['Lesson_1','Lesson_2','Lesson_3','Lesson_4','Lesson_5',]
+    // 'cambridge' : ['Cambridge_20', 'Cambridge_19', 'Cambridge_18', 'Cambridge_17', 'Cambridge_16', 'Cambridge_15', 'Cambridge_14', 'Cambridge_13', 'Cambridge_12', 'Cambridge_11', 'Cambridge_10'],
+    'cambridge' : ['Listening','Reading','Writing','Speaking'],
+    'collins' : ['Speaking_For_IELTS','Litening_For_IELTS','Reading_For_IELTS','Writing_For_IELTS'],
+    'grammarInUse' : ['Sorry, this book is still not added.'],
+    '504': ['Lesson_1', 'Lesson_2', 'Lesson_3', 'Lesson_4', 'Lesson_5', 'Lesson_6', 'Lesson_7', 'Lesson_8', 'Lesson_9', 'Lesson_10','Lesson_11', 'Lesson_12', 'Lesson_13', 'Lesson_14', 'Lesson_15','Lesson_16', 'Lesson_17', 'Lesson_18', 'Lesson_19', 'Lesson_20','Lesson_21', 'Lesson_22', 'Lesson_23', 'Lesson_24', 'Lesson_25','Lesson_26', 'Lesson_27', 'Lesson_28', 'Lesson_29', 'Lesson_30','Lesson_31', 'Lesson_32', 'Lesson_33', 'Lesson_34', 'Lesson_35','Lesson_36', 'Lesson_37', 'Lesson_38', 'Lesson_39', 'Lesson_40','Lesson_41', 'Lesson_42']
+}
+let subBooks = {
+    // Cambridge
+    'Cambridge_20' : ['Test_1','Test_2','Test_3','Test_4'],
+    'Cambridge_19' : ['Test_1','Test_2','Test_3','Test_4'],
+    'Cambridge_18' : ['Test_1','Test_2','Test_3','Test_4'],
+    'Cambridge_17' : ['Test_1','Test_2','Test_3','Test_4'],
+    'Cambridge_16' : ['Test_1','Test_2','Test_3','Test_4'],
+    'Cambridge_15' : ['Test_1','Test_2','Test_3','Test_4'],
+    'Cambridge_14' : ['Test_1','Test_2','Test_3','Test_4'],
+    'Cambridge_13' : ['Test_1','Test_2','Test_3','Test_4'],
+    'Cambridge_12' : ['Test_1','Test_2','Test_3','Test_4'],
+    'Cambridge_11' : ['Test_1','Test_2','Test_3','Test_4'],
+    'Cambridge_10' : ['Test_1','Test_2','Test_3','Test_4'],
+    // Collins
+
 }
 
-
-
-
 function navSelectionOpener(category , topic) {
-    let navSelection_element_children = navSelection_element.querySelectorAll('li')
     if (category == 'books') {
         switch (topic) {
 
             case 'cambridgeIELTS': 
                 navSelectionTitle_element.textContent = 'Cambridge IELTS'
-                bookMaker('cambridge')
+                navSelectionTitle_element__SAVE = 'Cambridge IELTS'
+                isSubBook_condition = false
+                bookMaker('cambridge' , false)
             break;
     
             case 'collins':
                 navSelectionTitle_element.textContent = 'Collins'
-                bookMaker('collins')
+                navSelectionTitle_element__SAVE = 'Collins'
+                isSubBook_condition = false
+                bookMaker('collins' , false)
             break;
 
             case 'grammarInUse' :
                 navSelectionTitle_element.textContent = 'Grammar In Use'
-                bookMaker('grammarInUse')
+                bookMaker('grammarInUse' , false)
+                isSubBook_condition = false
+                navSelectionTitle_element__SAVE = 'Grammar In Use'
             break;
 
             default:
                 navSelectionTitle_element.textContent = "Still not available"
+                navSelectionTitle_element__SAVE = "Still not available"
             break;
         }
     } else if (category == 'vocabulary') {
-        
+        switch (topic) {
+
+            case '504': 
+                navSelectionTitle_element.textContent = '504 Absolutely Essential Words'
+                navSelectionTitle_element__SAVE = '504 Absolutely Essential Words'
+                isSubBook_condition = false
+                bookMaker('504' , false)
+            break;
+    
+            case 'E_TOFEL':
+                navSelectionTitle_element.textContent = 'Essential Words For The TOEFL'
+                navSelectionTitle_element__SAVE = 'Essential Words For The TOEFL'
+                isSubBook_condition = false
+                bookMaker('E_TOFEL' , false)
+            break;
+
+            default:
+                navSelectionTitle_element.textContent = "Still not available"
+                navSelectionTitle_element__SAVE = "Still not available"
+            break;
+        }    
     } else if (condition== 'camShortcuts') {
         
     }
@@ -60,14 +106,35 @@ function navSelectionOpener(category , topic) {
         isSectionOpen = true
     }
 
-    function bookMaker(book) {
+
+
+    function bookMaker(book , isSubBooks) {
         navSelection_body_element.style.opacity = 0
+        navSelection_backBTN_element.style.opacity = 0
         setTimeout(() => {
+            navSelectionTitle_element.innerHTML = navSelectionTitle_element__SAVE
             navSelection_body_element.innerHTML = ''
             if (book in books) {
-                books[book].forEach((book) => {
-                    navSelection_body_element.innerHTML += `<li id="${book}">${book.replace('_' , ' ')}</li>`
+                books[book].forEach((bookItem) => {
+                    if (navSelection_body_element.id != bookItem) {
+                        if (navSelection_body_element.id == 'none') {
+                            navSelection_body_element.innerHTML += `<li id="${bookItem}"><a href="pages/${bookItem}.html">${bookItem.replaceAll("_", " ")}<a></li>`
+                        } else {
+                            navSelection_body_element.innerHTML += `<li id="${bookItem}"><a href="${bookItem}.html">${bookItem.replaceAll("_", " ")}<a></li>`
+                        }
+                    } else {
+                        navSelection_body_element.innerHTML += `<li style="opacity : .5;" id="${bookItem}">${bookItem.replaceAll("_", " ")}</li>`
+                    }
                     navSelection_body_element.style.opacity = 1
+                    if (isSubBooks == true) {
+                        navSelection_body_element.querySelectorAll('li').forEach((bookChild) => {
+                            bookChild.addEventListener('click' , (e) => {
+                                navSelection_body_element__SAVE = book
+                                navSelectionTitle_element.textContent = e.target.id.replaceAll("_", " ")
+                                subBookMaker(e.target.id)           
+                            })
+                        })
+                    }
                 })
             } else {
                 navSelection_body_element.innerHTML = '<li>This book is still not available.</li>'      
@@ -75,8 +142,28 @@ function navSelectionOpener(category , topic) {
             }
         }, 200);
     }
-}   
 
+    function subBookMaker(bookID) {
+        navSelection_backBTN_element.style.opacity = 1
+        navSelection_backBTN_element.addEventListener('click' , () => {
+            bookMaker(navSelection_body_element__SAVE , isSubBook_condition )
+        })
+        navSelection_body_element.style.opacity = 0
+        setTimeout(() => {
+            navSelection_body_element.innerHTML = ''
+            if (bookID in subBooks) {
+                subBooks[bookID].forEach((subBook) => {
+                    navSelection_body_element.innerHTML += `<li id="${subBook}">${subBook.replaceAll("_"," ")}</li>`
+                    navSelection_body_element.style.opacity = 1
+                })
+            } else {
+                navSelection_body_element.innerHTML = '<li>This book is still not available.</li>'
+                navSelection_body_element.style.opacity = 1
+            }
+            navSelection_backBTN_element
+        }, 200);
+    }
+}  
 navSelection_BTN_element.addEventListener('click' , () => {
     if (isSectionOpen) {
         closingFunc()
@@ -97,64 +184,22 @@ function closingFunc() {
         navSelection_element.style.overflow = 'hidden'
         setTimeout(() => {
             navSelection_element.style.height = 0
+            setTimeout(() => {
+                navSelection_body_element.innerHTML = ''
+            }, 300);
         }, 100);
     }, 200);
 } 
 
-// Book Opener
 
-let booksPreviewlist = document.querySelectorAll('.books_cambridge-books span');
-let activeBook = null;
+// Hero Section
 
-booksPreviewlist.forEach(book => {
-    book.addEventListener('click', (event) => {
-        event.stopPropagation();
-        
-        let buttons = book.querySelectorAll('button');
-        let paragraph = book.querySelector('p');
+// let hero_title = document.querySelector('.hero_title')
+// let firstScroll = false
+// window.addEventListener('scroll' , (e) => {
 
-        if (activeBook && activeBook !== book) {
-            resetBook(activeBook);
-        }
+//     hero_title.classList.add('hero_title-animation')
+//     hero_title.style.opacity = 1
+//     firstScroll = true
+// })
 
-        let isActive = book.classList.contains('bookActiveWidth');
-
-        if (!isActive) {
-            book.classList.add('bookActiveWidth');
-            buttons.forEach(btn => btn.classList.add('bookActiveButtons'));
-            paragraph.classList.add('bookActiveP');
-            activeBook = book;
-        } else {
-            resetBook(book);
-        }
-    });
-});
-
-document.addEventListener('click', () => {
-    if (activeBook) {
-        resetBook(activeBook);
-        activeBook = null;
-    }
-});
-
-function resetBook(book) {
-    book.classList.remove('bookActiveWidth');
-    let buttons = book.querySelectorAll('button');
-    let paragraph = book.querySelector('p');
-
-    buttons.forEach(btn => btn.classList.remove('bookActiveButtons'));
-    paragraph.classList.remove('bookActiveP');
-}
-
-
-
-//  Window Things
-
-let hero_title = document.querySelector('.hero_title')
-let firstScroll = false
-window.addEventListener('scroll' , (e) => {
-
-    hero_title.classList.add('hero_title-animation')
-    hero_title.style.opacity = 1
-    firstScroll = true
-})
